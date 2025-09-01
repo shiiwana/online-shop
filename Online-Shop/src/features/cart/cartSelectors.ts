d0d30929796aa2dbaa7489eebd7bd31f9c1d6478
shiiwana <sh3wem@gmail.com>
2025-09-01 12:08:28 +0330

    put try and catch for main.ts and create cartslice file for managing cartitems and also create cartselectors for managing states

---
+++ Online-Shop/src/features/cart/cartSelectors.ts
@@ -0,0 +1,11 @@
+import { type RootState } from "../../app/store";
+import type { CartItem } from "./cartSlice"; // 👈 نوع رو ایمپورت کن
+
+export const selectCartItems = (state: RootState) => state.cart.items;
+
+export const selectCartCount = (state: RootState) =>
+  state.cart.items.reduce((total: number, item: CartItem) => total + item.quantity, 0);
+
+export const selectCartTotal = (state: RootState) =>
+  state.cart.items.reduce((total: number, item: CartItem) => total + item.price * item.quantity, 0);
+
--- Online-Shop/src/main.tsx
+++ Online-Shop/src/main.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+const container = document.getElementById('root')
+
+if (container) {
+  const root = createRoot(container)
+
+  root.render(
     <Provider store={store}>
       <App />
-    </Provider>
-  </React.StrictMode>
-);
+    </Provider>,
+  )
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  )
+}
